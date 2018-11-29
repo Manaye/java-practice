e9687627207ad6618dd32ba8255daa0feeb4bc32
Manaye <hiwotmanaye2@gmail.com>
2018-11-29 13:18:06 -0800

    hasset

--- src/basiclibrary/linter/src/main/java/linter.java
+++ src/basiclibrary/linter/src/main/java/linter.java
@@ -5,12 +5,12 @@ import java.util.Iterator;
 public class linter {
 
     public static void main(String[] args) {
-              System.out.println(getMaxValue(new int[]{1,2,3,4}));
-              System.out.println(getMinValue(new int[]{1,2,3,4}));
+        System.out.println(getMaxValue(new int[]{1, 2, 3, 4}));
+        System.out.println(getMinValue(new int[]{1, 2, 3, 4}));
         iterateArrayofArray();
     }
 
-         public static int getMaxValue(int[] array) {
+    public static int getMaxValue(int[] array) {
 //             int maxValue = array[0];
 //             for (int i = 1; i < array.length; i++) {
 //                 if (array[i] > maxValue) {
@@ -18,29 +18,34 @@ public class linter {
 //                 }
 //             }
 //             return maxValue;
-             Arrays.sort(array);
+        Arrays.sort(array);
 
-             int max = array[array.length-1];
-             return  max;
-         }
+        int max = array[array.length - 1];
+        return max;
+    }
 
-// getting the miniumum value
-         public static int getMinValue(int[] array) {
+
+    // getting the miniumum value
+    public static int getMinValue(int[] array) {
 //             int minValue = array[0];
 //             for (int i = 1; i < array.length; i++) {
 //                 if (array[i] < minValue) {
 //                     minValue = array[i];
 //                 }
 //             }
-//             return minValue;
-             Arrays.sort(array);
+//             return minValue;S
+        Arrays.sort(array);
 
-             int min = array[0];
-             return  min;
-         }
+        int min = array[0];
+        System.out.println(min);
+
+        return min;
+
+    }
 
 
-    public static void iterateArrayofArray(){
+
+    public static void iterateArrayofArray() {
         int[][] weeklyMonthTemperatures = {
                 {66, 64, 58, 65, 71, 57, 60},
                 {57, 65, 65, 70, 72, 65, 51},
@@ -48,25 +53,26 @@ public class linter {
                 {65, 56, 55, 52, 55, 62, 57}
         };
 
-
-
-         HashSet<Integer> h = new HashSet<>();
-        for(int i = 0; i<weeklyMonthTemperatures.length; i++ ){
-            for(int j = 0; j<weeklyMonthTemperatures[i].length; j++){
+        HashSet<Integer> h = new HashSet<>();
+        for (int i = 0; i < weeklyMonthTemperatures.length; i++) {
+            for (int j = 0; j < weeklyMonthTemperatures[i].length; j++) {
                 System.out.println(weeklyMonthTemperatures[i][j]);
-//                h.add(weeklyMonthTemperatures[i][j]);
+          h.add(weeklyMonthTemperatures[i][j]);
 
 
+
+            }
+        Iterator<String> i = h.iterator();
+        while (i.hasNext())
+            System.out.println(i.next());
         }
-//        Iterator<String> i = h.iterator();
-//        while (i.hasNext())
-//            System.out.println(i.next());
     }
+}
 
 
 
 
 
 
-    }
+
 
